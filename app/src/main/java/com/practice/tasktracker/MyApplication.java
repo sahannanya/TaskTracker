d6f42dec33e0c84469592f9b93c79125d0253c82
Annanya <sahannanya@gmail.com>
2022-01-15 01:51:27 +0530

    logs exported to file

--- app/src/main/java/com/practice/tasktracker/MyApplication.java
+++ app/src/main/java/com/practice/tasktracker/MyApplication.java
@@ -1,29 +1,77 @@
 package com.practice.tasktracker;
 
 import android.app.Application;
-//import android.content.Context;
-//
-//import com.facebook.  stetho.Stetho;
-//import com.squareup.leakcanary.LeakCanary;
-//import com.squareup.leakcanary.RefWatcher;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
 
 @SuppressWarnings("ALL")
 public class MyApplication extends Application {
-    //    private RefWatcher refWatcher;
+    static private final String TAGm="MyApplication";
+    private String filePath;
 
     @Override
     public void onCreate() {
         super.onCreate();
-//        Stetho.initializeWithDefaults(this);
-//        if (LeakCanary.isInAnalyzerProcess(this)) {
-//            return;
-//        }
-//        refWatcher = LeakCanary.install(this);
+        startLoggingToFileSystem();
     }
 
-//    public static RefWatcher getRefWatcher(Context context) {
-//        MyApplication application = (MyApplication) context.getApplicationContext();
-//        return application.refWatcher;
-//    }
-//
+    /**
+     * reference - https://stackoverflow.com/a/37541596
+     */
+    private void startLoggingToFileSystem() {
+        Log.d(TAGm, "startLoggingToFileSystem() called");
+        if ( isExternalStorageWritable() ) {
+
+            File appDirectory = new File( getExternalFilesDir(null) + "/TaskTracker" );
+            File logDirectory = new File( appDirectory + "/logs" );
+            File logFile = new File( logDirectory, "logcat_" + System.currentTimeMillis() + ".txt" );
+
+            // create app folder
+            if ( !appDirectory.exists() ) {
+                appDirectory.mkdir();
+            }
+
+            // create log folder
+            if ( !logDirectory.exists() ) {
+                logDirectory.mkdir();
+            }
+
+            // clear the previous logcat and then write the new one to the file
+            try {
+                Process process = Runtime.getRuntime().exec("logcat -c");
+                process = Runtime.getRuntime().exec("logcat -f " + logFile);
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+
+        } else if ( isExternalStorageReadable() ) {
+            // only readable
+        } else {
+            // not accessible
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if ( Environment.MEDIA_MOUNTED.equals( state ) ) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if ( Environment.MEDIA_MOUNTED.equals( state ) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals( state ) ) {
+            return true;
+        }
+        return false;
+    }
+
 }
